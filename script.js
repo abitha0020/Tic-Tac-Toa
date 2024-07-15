1f48ec50a2dba84af35b89bba440c1b26ebe7a63
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-15 16:36:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function getQueryParams() {
     const params = new URLSearchParams(window.location.search);
     return {
-        player1: params.get('player1'),
-        player2: params.get('player2')
+        person1: params.get('player1'),
+        person2: params.get('player2')
     };
 }
 
-const { player1, player2 } = getQueryParams();
+const { person1, person2 } = getQueryParams();
 const playerNamesElement = document.getElementById('playerNames');
-playerNamesElement.textContent = `Player 1: ${player1} (X), Player 2: ${player2} (O)`;
+playerNamesElement.textContent = `Player 1: ${person1} (X), Player 2: ${person2} (O)`;
 
+function Player(name, marker){
+    this.name=name;
+    this.marker=marker; 
+}
+Player.prototype.returnName = function() {
+    return (this.name);
+}; 
+const player1 = new Player(person1,"X");
+const player2 = new Player(person2,"O");
+console.log(player1.returnName());
+console.log(player2.returnName());
+console.log(Object.getPrototypeOf(player2) === Player.prototype); 
+    
 const boardElement = document.getElementById('board');
 const cells = document.querySelectorAll('.cell');
 const restartButton = document.getElementById('restartButton');
@@ -38,25 +51,25 @@ const handleCellClick = (e) => {
         return;
     }
 
-    board[index] = currentPlayer;
-    cell.innerText = currentPlayer;
+    board[index] = currentPlayer.marker;
+    cell.innerText = currentPlayer.marker;
 
     if (checkWin()) {
-        statusElement.innerText = `Player ${currentPlayer} wins!`;
+        statusElement.innerText = `Player ${currentPlayer.name} wins!`;
         isGameActive = false;
     } else if (board.every(cell => cell !== '')) {
         statusElement.innerText = `It's a draw!`;
         isGameActive = false;
     } else {
         currentPlayer = currentPlayer === player1 ? player2 : player1;
-        statusElement.innerText = `Player ${currentPlayer}'s turn`;
+        statusElement.innerText = `Player ${currentPlayer.name}'s turn`;
     }
 };
 
 const checkWin = () => {
     return winningConditions.some(condition => {
         return condition.every(index => {
-            return board[index] === currentPlayer;
+            return board[index] === currentPlayer.marker;
         });
     });
 };
@@ -65,10 +78,10 @@ const restartGame = () => {
     board = ['', '', '', '', '', '', '', '', ''];
     currentPlayer = player1;
     isGameActive = true;
-    statusElement.innerText = `Player ${currentPlayer}'s turn`;
+    statusElement.innerText = `Player ${currentPlayer.name}'s turn`;
     cells.forEach(cell => cell.innerText = '');
 };
 
 cells.forEach(cell => cell.addEventListener('click', handleCellClick));
 restartButton.addEventListener('click', restartGame);
-statusElement.innerText = `Player ${currentPlayer}'s turn`;
+statusElement.innerText = `Player ${currentPlayer.name}'s turn`;
