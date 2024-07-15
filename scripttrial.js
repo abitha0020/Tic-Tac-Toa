0b5ece54cdae933b6c8ca32a195f9502b8d6ebde
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-15 15:10:02 +0530

    Update scripttrial.js

--- scripttrial.js
+++ scripttrial.js
@@ -3,11 +3,11 @@ const person2Input=document.getElementById("Person2");
 const startButton=document.getElementById("Startgame");
 function Player(name, marker){
     this.name=name;
-    this.marker=marker;
-    this.returnName= function() {
-        return (this.name);
-    };
+    this.marker=marker; 
 }
+Player.prototype.returnName = function() {
+    return (this.name);
+};  
 startButton.addEventListener('click', () => {
     const person1Name=person1Input.value.trim();
     const person2Name=person2Input.value.trim();
@@ -19,8 +19,7 @@ startButton.addEventListener('click', () => {
     const player2 = new Player(person2Name,"O");
     console.log(player1.returnName());
     console.log(player2.returnName());
-    console.log(Object.getPrototypeOf(player1)); // returns true
-    Object.getPrototypeOf(player2) === Player.prototype; // returns true
+    console.log(Object.getPrototypeOf(player2) === Player.prototype); // returns true
 
 
 })
